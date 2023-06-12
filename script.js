b9c83519233e0fb81f22df7e60d5c8e9f94cfca4
Abigail Logan <abbey@breadenfishmedia.com>
2023-06-11 21:59:07 -0500

    pasted functions

---
+++ script.js
@@ -0,0 +1,107 @@
+const choices = ["Rock", "Paper", "Scissors"];
+let pWinCount = 0;
+let cWinCount = 0;
+
+const buttons = document.querySelectorAll("button");
+const roundResult = document.querySelector(".roundResult");
+const gameResultDiv = document.querySelector("#gameResult");
+const pScore = document.querySelector(".pScore span");
+const cScore = document.querySelector(".cScore span");
+const resultContainer = document.querySelector("#main .row.results");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", getPlayerChoice);
+    btn.addEventListener("transitionend", removeRotation);
+});
+roundResult.addEventListener("transitionend", removeScale);
+
+function removeRotation(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("chosen");
+}
+
+function removeScale(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("announced");
+}
+
+function getPlayerChoice() {
+    if (gameResultDiv.textContent) return;
+    const playerSelection = this.dataset.choice;
+    const computerSelection = getComputerChoice();
+
+    roundResult.textContent = playRound(playerSelection, computerSelection);
+    pScore.textContent = pWinCount;
+    cScore.textContent = cWinCount;
+    this.classList.add("chosen");
+    roundResult.classList.add("announced");
+    checkForWinner();
+}
+
+function checkForWinner() {
+    if (pWinCount == 5 || cWinCount == 5) {
+        gameResultDiv.textContent = getWinner(pWinCount, cWinCount);
+
+        const playAgainBtn = document.createElement("button");
+        playAgainBtn.classList.add("playAgainBtn")
+        playAgainBtn.textContent = "Play again?";
+        playAgainBtn.addEventListener("click", resetGame);
+
+        resultContainer.appendChild(playAgainBtn);
+    }
+}
+
+function resetGame() {
+    pWinCount = 0;
+    cWinCount = 0;
+    pScore.textContent = pWinCount;
+    cScore.textContent = cWinCount;
+    gameResultDiv.textContent = "";
+    roundResult.textContent = "Click on a button to make a choice!";
+
+    resultContainer.removeChild(document.querySelector(".playAgainBtn"));
+    document.body.style.background = "rgb(53, 53, 53)";
+    buttons.forEach((btn) => {
+        if (btn.classList.contains("chosen")) btn.classList.remove("chosen");
+    })
+    if (roundResult.classList.contains("announced")) roundResult.classList.remove("announced");
+}
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random()*choices.length);
+    const choice = choices[randomNum];
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        changeBg("tie");
+        return `It's a tie! ${playerSelection} ties with ${computerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        pWinCount++;
+        changeBg("won");
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        pWinCount++;
+        changeBg("won");
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        pWinCount++;
+        changeBg("won");
+        return "You win! Scissors beats Paper";
+    } else {
+        cWinCount++;
+        changeBg("lost");
+        return `You lose! ${computerSelection} beats ${playerSelection}`;    
+    }
+}
+
+function getWinner(pWin, cWin) {
+    const winText = `You're the winner! You won the game!`;
+    const loseText = `You lost the game!`;
+    const tieText = `The game ended with a tie!`;
+
+    if (pWin > cWin) return winText;
+    else if (pWin == cWin) return tieText;
+    else return loseText;
+}
